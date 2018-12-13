a4f02499c7053403a57ffc854dec1c93322342b0
Vincent Baylly <vincent.baylly@computhand.com>
2018-12-13 11:33:12 -0500

    Integrate the template resume with CSS and responsive behaviour

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { HomeComponent } from './components/home/home.component';
 import { DiagramComponent } from './components/diagram/diagram.component';
 import { SysDiagramComponent } from './components/sys-diagram/sys-diagram.component';
 import { DevDiagramComponent } from './components/dev-diagram/dev-diagram.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { LicenseComponent } from './components/license/license.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent },
   { path: 'diagram', component: DiagramComponent },
   { path: 'devdiagram', component: DevDiagramComponent },
-  { path: 'sysdiagram', component: SysDiagramComponent }
+  { path: 'sysdiagram', component: SysDiagramComponent },
+  { path: 'termOfService', component: LicenseComponent }
 ];
 
 @NgModule({
