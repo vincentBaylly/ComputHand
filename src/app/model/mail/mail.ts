ae550b003aa5d13bad3982bb735e6cf8c5e3c692
Vincent Baylly <vincent.baylly@computhand.com>
2019-01-01 19:40:47 -0500

    Create the contact form page Add mailgun management Add email sending service

---
+++ src/app/model/mail/mail.ts
@@ -0,0 +1,46 @@
+import { MailConfig } from '../../model/mail/mail-config';
+
+export interface Mail {
+  from: string;
+  to?: string;
+  name?: string;
+  subject?: string;
+  text?: string;
+}
+
+/**
+* Create the email to send
+*/
+export function createMail(mail: Mail): { from: string; name: string; text: string; } {
+  let emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+  let mailToSend = { from: '', to: '', name: '', subject: '', text: '' };
+
+  try {
+
+    mailToSend.to = MailConfig.to;
+
+    if (mail.from.match(emailPattern)) {
+      mailToSend.from = mail.from;
+    } else {
+      throw 'the email provide is not valid';
+    }
+
+    if (mail.name.length < 3) {
+      mailToSend.subject = MailConfig.subject + ' ' + mail.name;
+    } else {
+      throw 'Name is required and needs to be at least 3 characters long.';
+    }
+
+    if(mail.text.length < 150){
+      mailToSend.text = mail.text;
+    }else{
+      throw 'The mail text is to long';
+    }
+
+  }
+  catch (e) {
+    console.log(e);
+  }
+
+  return mailToSend;
+}
---
+++ src/app/model/mail/mailgun-response.ts
@@ -0,0 +1,6 @@
+export class MailGunResponse{
+  id:string;
+  message:string;
+
+  constructor() {}
+}
