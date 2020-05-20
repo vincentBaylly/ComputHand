9228c7672d45f07d8851b0f24d63972afaed0868
Vincent Baylly <vincent.baylly@gmail.com>
2020-05-20 00:09:38 -0400

    add training content

---
+++ src/components/training/getConfluenceContent.sh
@@ -0,0 +1,29 @@
+AUTH=vincent.baylly@computhand.com:ldUbIVUMWpptbGUIe5bW4B55
+
+# Page SQLite Installation
+curl -bD \
+   -u $AUTH \
+   -X GET \
+   -H "Content-Type: application/json" \
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Installation+SQLite&expand=body.view" > info/installSQLiteContent.json
+
+# Page VueJS
+curl -bD \
+   -u $AUTH \
+   -X GET \
+   -H "Content-Type: application/json" \
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Vue.JS&expand=body.view" > vuejs/VueContent.json
+
+# Page Java Initial
+curl -bD \
+   -u $AUTH \
+   -X GET \
+   -H "Content-Type: application/json" \
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Java+Initial&expand=body.view" > java/JavaContent.json
+
+# Page Java pour le Web JEE J2EE Programmation Web en Java Hibernate
+curl -bD \
+   -u $AUTH \
+   -X GET \
+   -H "Content-Type: application/json" \
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Java+pour+le+Web+JEE+J2EE+Programmation+Web+en+Java+Hibernate&expand=body.view" > j2ee/J2EEContent.json
---
+++ src/components/training/java/Plan.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container content">
+    <h1>
+      <strong>Java Initial</strong>
+    </h1>
+    <ul>
+      <li>
+        <a href="#Mise en place de l’environnement de développement">
+          <u>Mise en place de l’environnement de développement</u>
+        </a>
+      </li>
+      <li>
+        <a href="#Syntaxe de programmation Java">
+          <u>Syntaxe de programmation Java</u>
+        </a>
+      </li>
+      <li>
+        <a href="#La programmation orientée objet">
+          <u>La programmation orientée objet</u>
+        </a>
+      </li>
+      <li>
+        <a href="#Exceptions dans le langage Java">
+          <u>Exceptions dans le langage Java</u>
+        </a>
+      </li>
+      <li>
+        <a href="#Gestion des entrées / sorties en Java">
+          <u>Gestion des entrées / sorties en Java</u>
+        </a>
+      </li>
+      <li>
+        <a href="#Collections & Fichiers">
+          <u>Collections & Fichiers</u>
+        </a>
+      </li>
+      <li>
+        <a href="#Java et les bases de données">
+          <u>Java et les bases de données</u>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
