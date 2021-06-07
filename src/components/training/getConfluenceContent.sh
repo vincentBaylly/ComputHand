42c651c04b7d0e144f1bbe97a79000eba197c0b2
vincentBaylly <vincent.baylly@gmail.com>
2021-06-07 13:40:48 -0400

    fix error on Atlassian content

--- src/components/training/getConfluenceContent.sh
+++ src/components/training/getConfluenceContent.sh
@@ -1,29 +1,28 @@
-AUTH=
-
+AUTH=vincent.baylly@computhand.com:ldUbIVUMWpptbGUIe5bW4B55
 # Page SQLite Installation
 curl -bD \
    -u $AUTH \
    -X GET \
    -H "Content-Type: application/json" \
-   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Installation+SQLite&expand=body.view" > info/installSQLiteContent.json
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Installation+SQLite&expand=body.view" >> src/components/training/content/installSQLiteContent.json
 
 # Page VueJS
 curl -bD \
    -u $AUTH \
    -X GET \
    -H "Content-Type: application/json" \
-   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Vue.JS&expand=body.view" > vuejs/VueContent.json
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Vue.JS&expand=body.view" >> src/components/training/content/VueContent.json
 
 # Page Java Initial
 curl -bD \
    -u $AUTH \
    -X GET \
    -H "Content-Type: application/json" \
-   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Java+Initial&expand=body.view" > java/JavaContent.json
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Java+Initial&expand=body.view" >> src/components/training/content/JavaContent.json
 
 # Page Java pour le Web JEE J2EE Programmation Web en Java Hibernate
 curl -bD \
    -u $AUTH \
    -X GET \
    -H "Content-Type: application/json" \
-   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Java+pour+le+Web+JEE+J2EE+Programmation+Web+en+Java+Hibernate&expand=body.view" > j2ee/J2EEContent.json
+   "https://computhand.atlassian.net/wiki/rest/api/content?spacekey=FD&title=Java+pour+le+Web+JEE+J2EE+Programmation+Web+en+Java+Hibernate&expand=body.view" >> src/components/training/content/J2EEContent.json
