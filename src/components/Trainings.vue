325419ed649a40d4d2ab052f1e1b905b3163e060
vincentBaylly <vincent.baylly@gmail.com>
2021-06-08 14:28:08 -0400

    Change trainings page display

--- src/components/Trainings.vue
+++ src/components/Trainings.vue
@@ -1,103 +1,127 @@
 <template>
   <div>
-    <div>
-      <div class="container">
-        <!-- <img src="/images/home.jpg" style="width:100%;height:100%"> -->
-      </div>
-    </div>
     <div class="container content">
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-          <h1>
-            <img src="/images/Technoworld.jpg" class="advicehood-logo" />
-            Le monde Techno:
-          </h1>
-          <ul>
-            <li>CyberSecurité</li>
-            <li>La cryptomonnaie</li>
-            <li>Modélisation 3D</li>
-          </ul>
-        </div>
-      </div>
-      <hr />
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-          <h1>
-            <img src="/images/Methodo.png" class="CommunityMapper-logo" />
-            Methodologie:
-          </h1>
-          <ul>
-            <li>Scrum</li>
-            <li>Agile Vision 360</li>
-            <li>
-              Gestion de projet Agile (<a
-                href="https://forceti.ca/formation-et-panel-ti/"
-                >Service au entreprise - Force TI</a
-              >)
-            </li>
-            <li>UML</li>
-          </ul>
-        </div>
-      </div>
-      <hr />
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-          <h1>
-            <img src="/images/Dev.jpg" class="MoRyIci-logo" />
-            Developpement:
-          </h1>
-          <ul>
-            <li>Initiation à la programmation</li>
-            <li>Initation au Javascript</li>
-            <ul>
-              <li>Framework:</li>
-              <ul>
-                <li>Angular</li>
-                <li>Vue</li>
-                <li>React</li>
-              </ul>
-            </ul>
-            <li>Java Initial</li>
-            <li>Java pour le Web, J2EE</li>
-            <ul>
-              <li>Framework:</li>
-              <ul>
-                <li>Hibernate</li>
-              </ul>
-            </ul>
-          </ul>
-        </div>
-      </div>
-      <hr />
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-          <h1>
-            <img src="/images/Tools.jpg" class="CommunityMapper-logo" />
-            Outils:
-          </h1>
-          <ul>
-            <li>Git</li>
-            <li>Maven</li>
-            <li>Jira</li>
-          </ul>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
-          <h1>
-            <img src="/images/Test.jpg" class="CommunityMapper-logo" />
-            Test:
-          </h1>
-          <ul>
-            <li>Selenium</li>
-            <li>SOAPUI</li>
-          </ul>
-        </div>
+      <div class="accordion text-left" role="tablist">
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" left class="p-1" role="tab">
+            <b-button v-b-toggle.accordion-1 variant="light"
+              ><b-icon-controller></b-icon-controller> Le monde
+              Techno:</b-button
+            >
+          </b-card-header>
+          <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <b-card-text>
+                <ul>
+                  <li>CyberSecurité</li>
+                  <li>La cryptomonnaie</li>
+                  <li>Modélisation 3D</li>
+                </ul></b-card-text
+              >
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button v-b-toggle.accordion-2 variant="light"
+              ><b-icon-calendar></b-icon-calendar> Methodologie:</b-button
+            >
+          </b-card-header>
+          <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <b-card-text>
+                <ul>
+                  <li>
+                    Scrum Master (<a
+                      href="https://www.doussou-formation.com/formation/formation-agile-scrum/"
+                      target="_blank"
+                      >Doussou Formation</a
+                    >)
+                  </li>
+                  <li>Agile Vision 360</li>
+                  <li>
+                    Gestion de projet Agile (<a
+                      href="https://forceti.ca/formation-et-panel-ti/"
+                      target="_blank"
+                      >Service au entreprise - Force TI</a
+                    >)
+                  </li>
+                  <li>UML</li>
+                </ul></b-card-text
+              >
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button v-b-toggle.accordion-3 variant="light"
+              ><b-icon-laptop></b-icon-laptop> Developpement</b-button
+            >
+          </b-card-header>
+          <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <b-card-text
+                ><ul>
+                  <li>Initiation à la programmation</li>
+                  <li>Initation au Javascript</li>
+                  <ul>
+                    <li>Framework:</li>
+                    <ul>
+                      <li>Angular</li>
+                      <li>Vue</li>
+                      <li>React</li>
+                    </ul>
+                  </ul>
+                  <li>Java Initial</li>
+                  <li>Java pour le Web, J2EE</li>
+                  <ul>
+                    <li>Framework:</li>
+                    <ul>
+                      <li>Hibernate</li>
+                    </ul>
+                  </ul>
+                </ul></b-card-text
+              >
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button v-b-toggle.accordion-4 variant="light"
+              ><b-icon-tools></b-icon-tools> Outils</b-button
+            >
+          </b-card-header>
+          <b-collapse id="accordion-4" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <b-card-text
+                ><ul>
+                  <li>Git</li>
+                  <li>Maven</li>
+                  <li>Jira</li>
+                </ul></b-card-text
+              >
+            </b-card-body>
+          </b-collapse>
+        </b-card>
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button v-b-toggle.accordion-5 variant="light"
+              ><b-icon-display></b-icon-display> Test</b-button
+            >
+          </b-card-header>
+          <b-collapse id="accordion-5" accordion="my-accordion" role="tabpanel">
+            <b-card-body>
+              <b-card-text
+                ><ul>
+                  <li>Selenium</li>
+                  <li>SOAPUI</li>
+                </ul></b-card-text
+              >
+            </b-card-body>
+          </b-collapse>
+        </b-card>
       </div>
     </div>
   </div>
